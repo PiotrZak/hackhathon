21e62380704573b9497ea84b63b45d51e7851898
Piotr Żak <piotrzak77@gmail.com>
2021-01-08 21:02:03 +0100

    material classifier

---
+++ materialClassifier.py
@@ -0,0 +1,23 @@
+
+
+public static void MaterialClassifier(){
+
+    def __init__(self):
+        materialType = None
+
+
+    String materialType;
+    def returnMaterial
+
+    switch(materialType){
+        case1 : materialType = "plastic";
+                     break;
+        case2 : materialType = "glass";
+                     break;
+        case3: materialType = "paper";
+                     break;
+        case4: materialType = "metal";
+                    break;
+    }
+    System.out.println(monthString);
+}
